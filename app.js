f8f9bdae26b154a41b2a96627d09ea01cbbb20f1
Anjali Ravindran <anjali93ravindran@gmail.com>
2022-05-05 09:41:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -47,5 +47,8 @@ app.get('/',function(req,res){
 //     });
 // });
 
-app.listen(5000);
+// app.listen(5000);
 // localhost:5000 or 127.0.0.1:5000
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
